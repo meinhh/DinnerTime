4b1ad7f23ccd96b074ea008034339e1a21fdf17b
Meirav Shenhar <meinhh@gmail.com>
2017-09-17 16:16:07 +0300

    server.js fixed

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-import express from 'express'
-import path from 'path'
-import serveStatic from 'serve-static'
-
-let app = express()
+var express = require('express')
+var path = require('path')
+var serveStatic = require('serve-static')
+var app = express()
 app.use(serveStatic(path.join(__dirname, 'dist')))
 var port = process.env.PORT || 5000
 app.listen(port)
