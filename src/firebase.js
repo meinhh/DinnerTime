fe8c24acdd9d3127e7615b29fa16eb14039ed1e7
Meirav Shenhar <meinhh@gmail.com>
2017-12-02 20:20:42 +0200

    something

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,11 @@
 import Firebase from 'firebase'
 const firebaseApp = Firebase.initializeApp({
-	apiKey: 'AIzaSyA6XH7WwcuX0eRBkVnnmaBqTpZRn3kOLdU',
+	apiKey: 'someapikey',
 	authDomain: 'la-vie-boheme.firebaseapp.com',
 	databaseURL: 'https://la-vie-boheme.firebaseio.com',
 	projectId: 'la-vie-boheme',
 	storageBucket: '',
-	messagingSenderId: '989171956124'
+	messagingSenderId: 'something'
 })
 const db = firebaseApp.database()
 export default db
